7b7dc43ae6a69cf9eaf71ed638fe25f2d0558d2b
oziel <dominguezvazquezoziel@gmail.com>
2019-08-07 03:07:54 -0500

    Holis

--- src/app/route/route.component.ts
+++ src/app/route/route.component.ts
@@ -4,9 +4,9 @@ import { AppComponent } from '../app.component';
 import { RouterModule, Routes } from '@angular/router';
 
 
-import { ProductsComponent } from '../components/products/products.component';
+import { ProductsComponent } from './../components/products/products.component';
 import { ServicesComponent } from '../components/services/services.component';
-//import { UsersComponent } from '../components/users/users.component';
+import { UsersComponent } from '../components/users/users.component';
 import { ClientsComponent } from '../components/clients/clients.component';
 import { Route } from '@angular/compiler/src/core';
 
@@ -15,7 +15,7 @@ const routes: Routes = [
     {path:'home', component: AppComponent},
     {path: 'products', component: ProductsComponent},
     {path: 'services', component: ServicesComponent},
-    //{path: 'users', component: UsersComponent},
+    {path: 'users', component: UsersComponent},
     {path: 'clients', component: ClientsComponent}
 
  
